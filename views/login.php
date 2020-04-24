d308dc744f84c4678d6fe2fbecbab2cddbdf68f5
Filipi Firmino <Filipi@localhost.localdomain>
2020-04-24 20:36:38 -0300

    style in documet adicionado

--- views/login.php
+++ views/login.php
@@ -1,8 +1,38 @@
-
-    <form  method="post">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        form{
+            height: 300px;
+            margin: 300px;
+            background-color: #ddd;
+            margin:auto;
+            margin-top: 30px;
+            padding: 20px;
+            border-radius:10px;
+        }
+        input{
+            width: 300px;
+            padding: 15px;
+            font-size: 14px;
+            border:1px solid #ddd;
+        }
+        
+    
+    
+    </style>
+</head>
+<body>
+<form  method="post">
         <input type="text" name="matricula" id="c_matricula" placeholder="ex: 0001"><br>
         <input type="password" name="senha" id="c_senha" placeholder="*****"><br>
         <input type="submit" value="Logar">
     </form>
+</body>
+</html>
+    
 
 
