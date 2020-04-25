84313ccc16ebbc8df0cb1a5f21ecda27c7ede2dc
Filipi Firmino <Filipi@localhost.localdomain>
2020-04-24 23:13:32 -0300

    end foreach

--- views/home.php
+++ views/home.php
@@ -1,15 +1,13 @@
 <div class="turmas">
     <h3>Sua turma</h3>
 
-    <?php
+    <?php  foreach($turmas  as $turmas):  ?>
 
-        foreach($turmas  as $turmas):
-
-
-    ?>
     <div class="turmasitem">
         <img src="" border="0" width="100%"/><br><br>
         <strong><?php echo $curso['nome'];?></strong><br/><br/>
 
     </div>
 </div>
+
+    <?php endforeach;?>
